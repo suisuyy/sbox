57db3a265216bbb1226c929eaeae7df70bdb5b8a
suisuy <suisuy@qq.com>
2022-08-25 10:08:37 +0800

    Update linuxpkg/novnc/installnovnc.sh

--- linuxpkg/novnc/installnovnc.sh
+++ linuxpkg/novnc/installnovnc.sh
@@ -3,7 +3,7 @@ ARCH=amd64
 unzip ./novnc1.3.0.zip
 sudo apt update 
 sudo apt install -y   gcc  fonts-wqy-zenhei firefox  xorg xfce4  xterm xfce4-terminal
-apt install ./turbovnc_3.0.1_amd64.deb ./n2n_3.0.0-1038_amd64.deb
+apt install ./turbovnc_3.0.1_$ARCH.deb ./n2n_3.0.0-1038_$ARCH.deb
 
 sudo edge -r -z1 -c suinet -k 080797ssY -a 192.168.100.200 -f -l 15.152.37.220:7777 -E 
 
