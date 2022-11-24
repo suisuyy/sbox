91acfea671069627ab032f3911132b4527e97ea4
Ubuntu <ubuntu@ip-172-31-36-117.eu-central-1.compute.internal>
2022-11-24 12:30:13 +0000

    fix caddy

--- app/vv/runserver.sh
+++ app/vv/runserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-
+/home/ubuntu/project/sbox/app/vv/caddy reverse-proxy --from awsg.suisuy.eu.org:443 --to :80 &
 /home/ubuntu/project/sbox/app/vv/v2ray -config /home/ubuntu/project/sbox/app/vv/wsserver.json
