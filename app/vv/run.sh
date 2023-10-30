89a4e19a5b59b0becfa2eb97102b0326e6089833
suisuyy <suisuyus@gmail.com>
2023-10-30 12:11:29 +0000

    backvmess

--- app/vv/run.sh
+++ app/vv/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 #/home/ubuntu/project/sbox/app/vv/caddy reverse-proxy --from awsg.suisuy.eu.org:443 --to :80 &
-./xray -config ./vlessws.json
-
+./v2ray -config ./wsserver.json
---
+++ app/xray/vv/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#/home/ubuntu/project/sbox/app/vv/caddy reverse-proxy --from awsg.suisuy.eu.org:443 --to :80 &
+./xray -config ./vlessws.json
+
