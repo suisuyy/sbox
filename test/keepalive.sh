90279d7669f2d3008e80a90ef8ef4f1f0dabe9f7
suisuyy <suisuyus@gmail.com>
2024-03-31 18:53:24 +0800

    Update keepalive.sh

--- test/keepalive.sh
+++ test/keepalive.sh
@@ -1,21 +1,10 @@
 
-# Define the URLs
-urls=(
-  "https://devilent-alistn.hf.space/"
-  "https://devilent-codeserver4.hf.space"
-  "https://devilent-testvv.hf.space"
-  "https://studious-halibut-9jgx5qvwj4rh9pr7.github.dev/"
-)
+
 
 # Loop through the URLs indefinitely
 while true
 do
-  for url in "${urls[@]}"
-  do
-    echo "Sending request to $url"
-    curl "$url" >/dev/null
-    echo $?
-  done
-  sleep 100  # Wait for 10 seconds before sending the next request
+  curl -s   https://raw.githubusercontent.com/suisuyy/sbox/master/test/awake.sh | bash
+  sleep 5  # Wait for 10 seconds before sending the next request
   
-done &
+done 
