91f922457c2149f880af92c1ac434af35c94bff0
suisuyy <suisuyus@gmail.com>
2024-03-31 19:03:50 +0800

    Update keepalive.sh

--- test/keepalive.sh
+++ test/keepalive.sh
@@ -6,5 +6,6 @@ while true
 do
   curl -s   https://raw.githubusercontent.com/suisuyy/sbox/master/test/awake.sh | bash
   sleep 100  # Wait for 10 seconds before sending the next request
+  sleep 3
   
 done 
